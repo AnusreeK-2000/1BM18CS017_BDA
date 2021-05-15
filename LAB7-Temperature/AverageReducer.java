6cd86045602cb05c89ce06b971d62e507000dd23
AnusreeK-2000 <53806481+AnusreeK-2000@users.noreply.github.com>
2021-05-15 23:11:30 +0530

    Update AverageReducer.java

--- LAB7-Temperature/AverageReducer.java
+++ LAB7-Temperature/AverageReducer.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 public class AverageReducer extends Reducer <Text, IntWritable, Text, IntWritable>{
 	
 	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-		int max_temp = 0;
+		int tot_temp = 0;
 		int count = 0;
 		for(IntWritable value: values) {
-			max_temp +=value.get();
+			tot_temp +=value.get();
 			count+=1;
 		}
-		context.write(key, new IntWritable(max_temp/count));
+		context.write(key, new IntWritable(tot_temp/count));
 	}
 
 }
